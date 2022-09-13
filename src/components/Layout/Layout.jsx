97d8117a8a28c56be40acecf7b3a561218f1653d
Dmitry Rakovich <dmitryrakovich@mail.ru>
2022-09-13 23:12:57 +0300

    add: header layout

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,21 @@
+import { NavLink, Link, Outlet } from "react-router-dom"
+import style from './Layout.module.css'
+
+const setActive = ({ isActive }) => isActive ? style.active : ""
+
+const Layout = () => {
+  return (
+    <>
+    <header className={style.header}>
+        <NavLink className={setActive} to="/">Home</NavLink>
+        <NavLink className={setActive} to="/news">News</NavLink>
+        <NavLink className={setActive} to="/profile">Profile</NavLink>
+    </header>
+    <main className={style.container}>
+        <Outlet />
+    </main>
+    </>
+  )
+}
+
+export {Layout}
