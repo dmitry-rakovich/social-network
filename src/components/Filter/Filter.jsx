e05c55786fb48a3e37e5a87f32d4514c00fb5a5f
Dmitry Rakovich <dmitryrakovich@mail.ru>
2022-09-18 18:51:23 +0300

    add: redux in component

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,18 +1,24 @@
-import React, { useState } from 'react'
-import { News } from '../News/News'
+import { useDispatch } from 'react-redux'
+import {filterNews} from '../../store/newsSlice'
+import style from '../News/News.module.css'
 
-const Filter = ({news, setNews}) => {
+const Filter = () => {
 
-  const filterNews = (value) => {
-    setNews(news.filter(item => item.category === value))
+  const dispatch = useDispatch()
+
+  const filterItems = (e) => {
+    document.querySelectorAll(`.${style.filter_button}`).forEach(item => item.classList.remove(`${style.active}`))
+    e.target.classList.add(`${style.active}`)
+    const category = e.target.value
+    dispatch(filterNews({category}))
   }
 
   return (
-    <div className='filter-buttons'>
-    <button onClick={(e) => filterNews(e.target.value)} value="">All</button>
-    <button onClick={(e) => filterNews(e.target.value)} value="Sport">Sport</button>
-    <button onClick={(e) => filterNews(e.target.value)} value="Social">Social</button>
-    <button onClick={(e) => filterNews(e.target.value)} value="Auto">Auto</button>
+    <div className={style.filter}>
+    <button className={`${style.filter_button} ${style.active}`} onClick={(e) => filterItems(e)} value="All">All</button>
+    <button className={style.filter_button} onClick={(e) => filterItems(e)} value="Sport">Sport</button>
+    <button className={style.filter_button} onClick={(e) => filterItems(e)} value="Technique">Technique</button>
+    <button className={style.filter_button} onClick={(e) => filterItems(e)} value="Auto">Auto</button>
     </div>
   )
 }
