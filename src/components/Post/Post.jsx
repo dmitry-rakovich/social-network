d8c7853b672d1689300b06d60e16505c280bd4c3
Dmitry Rakovich <dmitryrakovich@mail.ru>
2022-09-18 17:52:42 +0300

    add: reducers

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,61 +1,56 @@
 import {React, useState} from 'react'
+import { useDispatch } from 'react-redux'
+import { deletePost, editPost, savePost } from '../../store/postSlice'
 import style from './Post.module.css'
 
-const Post = ({post, posts, setPosts}) => {
+const Post = ({img, text, id, isEdit}) => {
+  
+  const dispatch = useDispatch()
+
   const [newImg, setNewImg] = useState('')
   const [newText, setNewText] = useState('')
-  const [isEdit, setIsEdit] = useState(false)
 
-  const savePost = (e) => {
-    e.preventDefault()
-    setPosts([
-      {
-        text:newText, img:newImg, id:Date.now()
-      },
-      ...posts.filter(item => item.id !== post.id)
-    ])
-  }
-
-  const editPost = (e, postId) => {
+  const editItem = (e) => {
     setNewImg(e.target.parentElement.parentElement.querySelector(`img`) ? e.target.parentElement.parentElement.querySelector(`img`).src :  '')
     setNewText(e.target.parentElement.parentElement.querySelector(`p`).innerText)
-    if(postId === post.id) {
-      setIsEdit(!isEdit)
-    }
+    dispatch(editPost({id}))
   }
 
-  const deletePost = (postId) => {
-    setPosts(posts.filter(item => item.id !== postId))
-}
+  const saveItem = (e) => {
+    if (e.target.previousElementSibling.checkValidity()) {
+      dispatch(savePost({id, newText, newImg}))
+      dispatch(editPost({id}))
+    } else e.target.previousElementSibling.reportValidity()
+  }
 
   return (
     <>
     {!isEdit && <div className={style.post}>
-      {post.img && <img src={post.img}/>}
-      <p className={style.text}>{post.text}</p>
+      {img && <img src={img} width='50%'/>}
+      <p className={style.text}>{text}</p>
       <div className={style.buttons}>
-        <button className={style.button} onClick={(e) => editPost(e, post.id)}>Edit</button>
-        <button className={style.button} onClick={() => deletePost(post.id)}>Delete</button>
+        <button className={style.button} onClick={(e) => editItem(e)}>Edit</button>
+        <button className={style.button} onClick={() => dispatch(deletePost({id}))}>Delete</button>
       </div>
     </div>}
       {isEdit &&
-              <form className={style.post}>
+              <div className={style.post}>
                 <textarea
                 className={style.textarea} 
                 onChange={(e)=> setNewText(e.target.value)} 
                 type="text" 
-                defaultValue={post.text}
+                defaultValue={text}
                 placeholder="Add new text"
                 />
                 <input
                 className={style.input}
                 onChange={(e)=> setNewImg(e.target.value)} 
                 type="url" 
-                defaultValue={post.img}
+                defaultValue={img}
                 placeholder="Add new image URL"
                 />
-                <button className={style.save_button} onClick={(e) => savePost(e)}>Save</button>
-            </form>
+                <button className={style.save_button} onClick={(e) => saveItem(e)}>Save</button>
+            </div>
       }
     </>
   )
--- src/pages/Profilepage.jsx
+++ src/pages/Profilepage.jsx
@@ -1,13 +1,55 @@
-import { Posts } from "../components/Posts/Posts"
-import style from '../components/Posts/Posts.module.css'
 import { Profile } from "../components/Profile/Profile"
+import { Post } from '../components/Post/Post'
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { addPost } from '../store/postSlice'
+
+import style from '../components/Profile/Profile.module.css'
+
 const Profilepage = () => {
+
+  const posts = useSelector(state => state.posts.posts)
+
+  const [text, setText] = useState('')
+  const [img, setImg] = useState('')
+  const [isShowForm, setShowForm] = useState(false)
+  const dispatch = useDispatch()
+  const addItem = (e) => {
+    if (text.trim()) {
+      if (e.target.previousElementSibling.checkValidity()) {
+      dispatch(addPost({text, img}))
+      setText('')
+      setImg('')
+      setShowForm(false)
+    } else e.target.previousElementSibling.reportValidity()} else {
+      e.target.previousElementSibling.previousElementSibling.value = ''
+      e.target.previousElementSibling.previousElementSibling.reportValidity()
+    }
+  }
+
   return (
     <>
-    <Profile/>
-    <div className={style.posts}>
-      <Posts/>
-    </div>
+      <Profile/>
+      <div className={style.posts}>
+        {posts.length ? <h1>Posts</h1> : <h1>No posts</h1>}
+        <button className={style.button_show_post} onClick={() => {setShowForm(!isShowForm)}}>Add post</button>
+        {isShowForm && 
+        <div className={style.form}>
+          <h2>Add post</h2>
+          <textarea className={style.textarea} required value={text} onChange={(e) => setText(e.target.value)} placeholder="Add text"/>
+          <input className={style.input} type="url" value={img} onChange={(e) => setImg(e.target.value)} placeholder="Add image URL"/>
+          <button className={style.button_show_post} onClick={(e) => addItem(e)}>Add post</button>
+        </div >
+        }
+        {
+        posts.concat().reverse().map(item => 
+        <Post
+        key={item.id}
+        {...item}
+        />
+        )
+        }
+      </div>
     </>
   )
 }
